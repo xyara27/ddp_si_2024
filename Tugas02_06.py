f56e9a3b63250dfc19636bc9de8d773bd063e4e9
xyara27 <auranajwa27@gmail.com>
2024-10-30 15:43:44 +0700

    TUGAS06_AURA NAJWA_0110124069_DDP

---
+++ Tugas02_06.py
@@ -0,0 +1,5 @@
+hasil = 0 
+for i in range(1,20,2):
+    hasil += i
+print(f"1 + 3 + 5 + 7 + 9 + 11 + 13 + 15 + 17 + 19 = {hasil}")
+
---
+++ Tugas03_06.py
@@ -0,0 +1,3 @@
+b = int(input("masukkan jumlah baris : "))
+for y in range(1, b+1):
+    print("*" * y) 
